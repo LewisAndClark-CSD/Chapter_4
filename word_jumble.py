3c00a75ed8f2c3cab76d6603698e43db1e32e8e4
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-28 04:19:38 -0500

    word_jumble.py

--- word_jumble.py
+++ word_jumble.py
@@ -28,16 +28,42 @@ print(
 (Press the enter key at the prompt to quit.)
 """
 )
+score = 100
 print("The jumble is:", jumble)
 
 guess = input("\nYour guess: ")
-while guess != correct and guess != "":
-    print("Sorry, that's not it.")
-    guess = input("Your guess: ")
-    
+
 if guess == correct:
-    print("That's it!  You guessed it!\n")
+    print("Wow you got it without a hint!")
+elif guess == "":
+    print("No guess?")
+elif guess == "hint":
+    score = score - 5
+    if correct == "python":
+        print("It's a kind of snake.")
+        input("Your guess: ")
+        score = score - 1
+    elif correct == "jumble":
+        print("Welcome to Word _____!")
+        input("Your guess: ")
+        score = score - 1
+    elif correct == "easy":
+        print("The opposite of hard.")
+        input("Your guess: ")
+        score = score - 1
+    elif correct == "difficult":
+        print("This word jumble is....")
+        input("Your guess: ")
+        score = score - 1
+    elif correct == "answer":
+        print("If someone ask a question you would _____ it.")
+        input("Your guess: ")
+        score = score - 1
+    elif correct == "xylophone":
+        print("An instrament that starts with x")
+        input("Your guess: ")
+        score = score - 1
 
-print("Thanks for playing.")
-
-input("\n\nPress the enter key to exit.")
+print("That's it you got it!")
+print("Thanks for playing!")
+print("Your score was",score,"out of 100!")
