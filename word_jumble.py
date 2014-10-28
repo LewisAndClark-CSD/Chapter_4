d79bd89d6bd53bb22124a6a461e426b8ee748170
LukeGosnell <LGosnell@CIS-06.CIS.COM>
2014-10-28 06:55:33 -0500

    Finished Chapter 4 Challenges

--- word_jumble.py
+++ word_jumble.py
@@ -1,3 +1,8 @@
+#! /usr/bin/python3
+# word_jumble.py
+# Luke Gosnell
+# 10/27/2014
+
 # Word Jumble
 #
 # The computer picks a random word and then "jumbles" it
@@ -11,9 +16,24 @@ WORDS = ("python", "jumble", "easy", "difficult", "answer", "xylophone")
 word = random.choice(WORDS)
 # create a variable to use later to see if the guess is correct
 correct = word
+amount = 0
+
+#Hints
+if word == "python":
+    hint = "Computer program"
+elif word == "jumble":
+    hint = "I just did it to these words"
+elif word == "easy":
+    hint = "This game is pretty _ _ _ _"
+elif word == "difficult":
+    hint = "This game isn't very _ _ _ _ _ _ _ _ _"
+elif word == "answer":
+    hint = "Opposite of problem"
+elif word == "xylophone":
+    hint = "Musical instrument"
 
 # create a jumbled version of the word
-jumble =""
+jumble = ""
 while word:
     position = random.randrange(len(word))
     jumble += word[position]
@@ -26,17 +46,28 @@ print(
         
    Unscramble the letters to make a word.
 (Press the enter key at the prompt to quit.)
-"""
-)
+""")
+
 print("The jumble is:", jumble)
+reward = ""
 
 guess = input("\nYour guess: ")
-while guess != correct and guess != "":
-    print("Sorry, that's not it.")
+while (guess != correct and guess != ''):
+    print("Sorry, thats not it.")
+    response = input("Would you like a hint? (y/n) ")
+    if response == "y":
+        amount = amount + 1
+        print(hint)
+    
+        
+    
     guess = input("Your guess: ")
     
 if guess == correct:
     print("That's it!  You guessed it!\n")
+
+    if amount == 0:
+        print("You solved it without a hint! Good job!")
 
 print("Thanks for playing.")
 
